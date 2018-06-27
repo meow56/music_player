d9436bb9d8380c0ee3570cedb2c8cc9c43f6c21d
meow56 <spinattacktotheface@gmail.com>
2018-06-26 20:01:10 -0500

    Basics complete, bar testing
    EZ

--- main.js
+++ main.js
@@ -7,17 +7,31 @@ function submitFiles() {
   for(var i = 0; i < newFiles.length; i++) {
     music.push(newFiles[i]);
   }
+  musicPlayed = [];
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function playMusic() {
   if(document.getElementById("current_music") !== null) {
     if(document.getElementById("current_music").ended) {
       document.removeChild(document.getElementById("current_music"));
-      
+      var musicTemp = music.slice();
+      for(var i = 0; i < musicPlayed.length; i++) {
+        for(var j = 0; j < musicTemp.length; j++) {
+          if(musicPlayed[i] === musicTemp[j]) {
+            musicTemp.splice(j, 1);
+          }
+        }
+      }
+      var musicToPlay = musicTemp[randomNumber(0, musicTemp.length - 1)];
       var temp = document.createElement("AUDIO");
       temp.file = musicToPlay;
       temp.controls = "true";
       temp.id = "current_music";
+      musicPlayed.push(musicToPlay);
       document.appendChild(temp);
     }
   }
